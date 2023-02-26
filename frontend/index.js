9c86b7e9e38946403aee9eb21b286d8fdb50cc23
Aleksei Baliuk <aleksred.b@gmail.com>
2023-02-26 13:38:58 +0200

    add-files

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+import alex from "./js/alex";
+import alex1 from "./js/alex1";
+// import "./css/alex.css";
+import "./sass/alex.scss";
+
+console.log(alex);
+console.log(alex1);
+
+console.log("message !!!!");
